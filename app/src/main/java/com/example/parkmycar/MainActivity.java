99c9bd010df3b0b0ab71213a5b65b68740676abc
Dhinesh Subramanian <dhinesh17035@cse.ssn.edu.in>
2020-03-15 22:14:24 +0530

    Added Welcome message

--- app/src/main/java/com/example/parkmycar/MainActivity.java
+++ app/src/main/java/com/example/parkmycar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.parkmycar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,10 +35,18 @@ public class MainActivity extends AppCompatActivity {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-        if(account!=null)
-            Toast.makeText(this,"Already signed in",Toast.LENGTH_SHORT).show();
+        if(account!=null) {
+//            Toast.makeText(this,"Already signed in",Toast.LENGTH_SHORT).show();
+            String personName = account.getDisplayName();
+//                String personGivenName = acct.getGivenName();
+//                String personFamilyName = acct.getFamilyName();
+//                String personEmail = acct.getEmail();
+//                String personId = acct.getId();
+            Uri personPhoto = account.getPhotoUrl();
 
-        signinbutton.setOnClickListener(new View.OnClickListener() {
+            Snackbar.make(findViewById(android.R.id.content),"Welcome Back "+personName, Snackbar.LENGTH_SHORT);
+        }
+            signinbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -56,20 +66,32 @@ public class MainActivity extends AppCompatActivity {
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+
         }
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+//            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+            if (account != null) {
+                String personName = account.getDisplayName();
+//                String personGivenName = acct.getGivenName();
+//                String personFamilyName = acct.getFamilyName();
+//                String personEmail = acct.getEmail();
+//                String personId = acct.getId();
+                Uri personPhoto = account.getPhotoUrl();
+                Snackbar.make(findViewById(android.R.id.content),"Welcome "+personName,Snackbar.LENGTH_LONG).show();
+            }
             // Signed in successfully, show authenticated UI.
             //updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Toast.makeText(this, "signInResult:failed code=" + e.getStatusCode(),Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "signInResult:failed code=" + e.getStatusCode()+e.getMessage(),Toast.LENGTH_LONG).show();
             //updateUI(null);
         }
     }
+
+
 }
